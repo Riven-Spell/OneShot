2ef5b59dbf7332619c6320a6d21ef0fe68029bbd
Adele Reed <Adele Reed>
2016-05-21 19:53:22 -0700

    added groundwork for bullet firing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,56 @@ using UnityEngine.Networking;
 public class Player : NetworkBehaviour {
     // Update is called once per frame
     public GameObject camera;
+    public GameObject bullet;
 	bool hasBullet = false;
 
-    public override void OnStartLocalPlayer() {\
+    bool isCharging = false;
+    int charged = 0;
+
+    public override void OnStartLocalPlayer() {
         camera.SetActive(true);
     }
 
+    void Start() {
+        InvokeRepeating("Charge", 0.0f, 1.0f);
+    }
 
-    void Update () {
+    void Charge()
+    {
+        if (isCharging)
+            charged += 1;
+    }
+
+    void Fire()
+    {
+        if (hasBullet)
+        {
+            
+        }
+    }
+
+    void Update() {
         if (!isLocalPlayer)
             return;
 
         gameObject.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse X"));
         gameObject.transform.position += gameObject.transform.forward * (Input.GetAxis("Vertical") * 0.1f);
         gameObject.transform.position += gameObject.transform.right * (Input.GetAxis("Horizontal") * 0.1f);
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            isCharging = true;
+            if (charged >= 9)
+            {
+                //Launch bullet.
+            }
+        }
+        else
+        {
+            isCharging = false;
+            //Launch bullet
+            charged = 0;
+        }
         //gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal") * 10, 0, Input.GetAxis("Vertical") * 10));
     }
 
@@ -27,5 +63,14 @@ public class Player : NetworkBehaviour {
 			hasBullet = true;
 			Destroy (collision.gameObject);
 		}
+
+        if (collision.gameObject.tag == "bullet")
+        {
+            //DEATH STRIKES!
+            camera.transform.SetParent(GameObject.Find("SpecPoint").transform);
+            camera.transform.localPosition = new Vector3(0, 0, 0);
+            camera.transform.localRotation = Quaternion.identity;
+            Destroy(gameObject);
+        }
 	}
 }
